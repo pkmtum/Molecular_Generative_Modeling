e46dc3bcbb77e9d7ea974874f3357bdf6922a20e
Sven Lüpke <luepke@in.tum.de>
2023-12-05 21:46:09 +0100

    Add graph autoencoder model

--- source/qm9/create_one_hot_to_atom_number_map.py
+++ source/qm9/create_one_hot_to_atom_number_map.py
@@ -17,8 +17,10 @@ def main():
     dataset = QM9(root="./data")
 
     one_hot_atom_num_map = {}
+    max_atom_count = 0
 
     for data in tqdm(dataset):
+        max_atom_count = max(max_atom_count, data.x.shape[0])
         for atom_features in data.x:
             one_hot = atom_features[:5]
             atomic_number = atom_features[5].item()
@@ -26,6 +28,7 @@ def main():
             one_hot_atom_num_map[class_index] = int(atomic_number)
         
     print(f"Found {len(one_hot_atom_num_map)} unique atoms!")
+    print(f"The largest molecule has {max_atom_count} atoms!")
     json_str = json.dumps(one_hot_atom_num_map, indent=4)
     with open(out_json_filename, "w") as outfile:
         outfile.write(json_str)
