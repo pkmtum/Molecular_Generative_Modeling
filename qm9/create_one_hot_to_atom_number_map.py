e6d5993af627b3da12bce5edceae20b1836ace87
Sven Lüpke <luepke@in.tum.de>
2023-12-05 16:20:35 +0100

    Add tensorboard visualization

---
+++ qm9/create_one_hot_to_atom_number_map.py
@@ -0,0 +1,34 @@
+import argparse
+import json
+
+from tqdm import tqdm
+import torch
+from torch_geometric.datasets import QM9
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Create a mapping from the one-hot encoded atomic number in the torch-geometric QM9 to the actual atomic number."
+    )
+    parser.add_argument("--out", default="qm9_hot_atomic_number.json", type=str, help="Output json file.")
+
+    args = parser.parse_args()
+    out_json_filename = args.out
+    
+    dataset = QM9(root="./data")
+
+    one_hot_atom_num_map = {}
+
+    for data in tqdm(dataset):
+        for atom_features in data.x:
+            one_hot = atom_features[:5]
+            atomic_number = atom_features[5].item()
+            class_index = torch.argmax(one_hot).item()
+            one_hot_atom_num_map[class_index] = int(atomic_number)
+        
+    print(f"Found {len(one_hot_atom_num_map)} unique atoms!")
+    json_str = json.dumps(one_hot_atom_num_map, indent=4)
+    with open(out_json_filename, "w") as outfile:
+        outfile.write(json_str)
+
+if __name__ == "__main__":
+    main()
