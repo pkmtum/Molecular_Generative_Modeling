e8245d9fdf42797b813c5c26d065a916730b0217
Sven Lüpke <luepke@in.tum.de>
2024-01-10 11:41:04 +0100

    Add ECCConv implementation

--- source/graph_vae/encoder.py
+++ source/graph_vae/encoder.py
@@ -3,26 +3,54 @@ from typing import Dict, Any, Tuple
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn import Linear, Parameter
 
 from torch_geometric.data import Data
-from torch_geometric.nn import GCNConv, global_mean_pool, BatchNorm
+from torch_geometric.nn import GCNConv, global_mean_pool, BatchNorm, EdgeConv
 from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.utils import degree
+
 
 class ECCConv(MessagePassing):
     r""" The graph convolution operator from the 
     `"Dynamic Edge-Conditioned Filters in Convolutional Neural Networks on Graphs"
     <https://arxiv.org/abs/1609.02907>`_ paper """
     
-    def __init__(self, in_channels: int, out_channels: int):
+    def __init__(self, num_edge_features: int, in_channels: int, out_channels: int):
         super().__init__(aggr='add')
 
-    def forward(self, x, ) -> Any:
-        # TODO
-        pass
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.edge_fc = Linear(num_edge_features, in_channels * out_channels, bias=False)
+        self.bias = Parameter(torch.empty(out_channels))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.edge_fc.reset_parameters()
+        self.bias.data.zero_()
+
+    def forward(self, x, edge_index, edge_attr):
+        theta = self.edge_fc(edge_attr)
+        theta = theta.view(-1, self.out_channels, self.in_channels)
+
+        # normalization factor
+        deg = degree(edge_index[0], x.size(0), dtype=x.dtype)
+        deg_rcp = 1.0 / deg
+        deg_rcp[deg_rcp == float("inf")] = 0
+        norm = deg_rcp.unsqueeze(-1)
+        
+        out = self.propagate(edge_index, x=x, norm=norm, theta=theta)
+
+        out += self.bias
+
+        return out
+
+    def message(self, x_j, norm_j, theta):
+        return norm_j * torch.bmm(theta, x_j.unsqueeze(-1)).squeeze(-1)
 
 
-class GraphAttentionPooling(nn.Module):
+class GlobalGraphPooling(nn.Module):
 
     def __init__(self) -> None:
         super().__init__()
@@ -40,6 +68,13 @@ class Encoder(nn.Module):
         self.latent_dim = hparams["latent_dim"]
 
         # TODO: two graph convolutional layers (32 and 64 channels) with identity connection (edge conditioned graph convolution)
+
+        self.ecc_conv = ECCConv(
+            num_edge_features=hparams["num_edge_features"],
+            in_channels=hparams["num_node_features"],
+            out_channels=32
+        )
+
         self.conv_1 = GCNConv(in_channels=hparams["num_node_features"], out_channels=32)
         self.batch_norm_1 = BatchNorm(in_channels=32)
         self.conv_2 = GCNConv(in_channels=32, out_channels=64)
@@ -53,6 +88,8 @@ class Encoder(nn.Module):
 
     def forward(self, data: Data) -> Tuple[torch.Tensor, torch.Tensor]:
         x, edge_index, batch, edge_attr = data.x, data.edge_index, data.batch, data.edge_attr
+
+        tmp = self.ecc_conv(x, edge_index, edge_attr)
 
         x = self.conv_1(x, edge_index)
         x = self.batch_norm_1(x)
